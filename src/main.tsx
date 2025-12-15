2e17c77cc16358654fb25ce82a79907127feed50
KaeTheDev <kaethedev@gmail.com>
2025-12-15 12:42:46 -0500

    Installed React Router and set up main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { FavoritesProvider } from './context/FavoritesContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
+<FavoritesProvider>
+<BrowserRouter>
   <StrictMode>
     <App />
   </StrictMode>,
+ </BrowserRouter>
+</FavoritesProvider>
 )
