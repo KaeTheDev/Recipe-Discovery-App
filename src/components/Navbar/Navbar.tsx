8275fb52acd75872ad6b0549f216675713e3fce7
KaeTheDev <kaethedev@gmail.com>
2025-12-16 05:30:03 -0500

    Added search bar to navbar and hover effect to nav links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,9 +5,10 @@ export const Navbar: React.FC = () => {
         <nav className="bg-black px-4 py-5">
             <div className="flex flex-row justify-between">
             <p className="text-3xl text-white">Recipe Discovery App</p>
+            <input className="border text-white px-4 py-4" type="text" placeholder="Search Recipe..." />
             <ul className="flex flex-row">
-                <li className="text-white"> <Link className="mr-3" to={"/"}>Home</Link></li>
-                <li className="text-white"><Link to={"/favorites"}>Favorites</Link></li>
+                <li className="text-white hover:text-orange-300"> <Link className="mr-3" to={"/"}>Home</Link></li>
+                <li className="text-white hover:text-orange-300"><Link to={"/favorites"}>Favorites</Link></li>
             </ul>
             </div>
         </nav>
