425c2133feb571f4d9dd568f6376499e15e50176
KaeTheDev <kaethedev@gmail.com>
2025-12-15 23:31:54 -0500

    Set up route and component for Favorites

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { Category } from "./pages/Category";
 import { RecipeDetail } from "./pages/RecipeDetail";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Favorites } from "./pages/Favorites";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/category/:categoryName" element={<Category />} />
         <Route path="/recipe/:recipeID" element={<RecipeDetail />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </>
   );
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,5 @@
+export const Favorites: React.FC = () => {
+    return (
+        <h1 className="text-4xl">FAVORITES PAGE</h1>
+    )
+}
